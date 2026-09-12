bae3f79c3d0ed1e2a606a7b3b894f8ead446a392
obabakir <bubufighting01@gmail.com>
2026-09-13 08:34:15 +0900

    feat: meta tags integrations on document private file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(" _APP is working ");
   return <Component {...pageProps} />;
 }
