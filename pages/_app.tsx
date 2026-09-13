fc6c491733c7b5f00811e44ceeb449fff1f48f5f
obabakir <bubufighting01@gmail.com>
2026-09-13 12:10:35 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+// @ts-ignore
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
