2540e3876e5b2cbb0680b5526356303d9f4f9180
obabakir <bubufighting01@gmail.com>
2026-09-14 23:51:11 +0900

    feat: integration apollo client & graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,25 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
 import { NextPage } from "next";
+// @ts-ignore
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+// @ts-ignore
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+// @ts-ignore
+import Advertisement from "@/libs/components/homepage/Advertisement";
+// @ts-ignore
+import TopProperties from "@/libs/components/homepage/TopProperties";
+// @ts-ignore
+import TopAgents from "@/libs/components/homepage/TopAgents";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack className={"home-page"}>
-        <Stack>
-          <Stack className="container">Trend Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Advertisement</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Proerties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-      </Stack>
+    <Stack className={"home-page"}>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
   );
 };
